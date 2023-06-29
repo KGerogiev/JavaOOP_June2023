240bc5ad05c9c93a0b28a55339c1e4da3ceaed8d
KGerogiev <kristian_kunev1@abv.bg>
2023-06-29 15:38:44 +0300

    Add code solution

---
+++ 03_Inheritance/lab/P01_SingleInheritance_LAB/Animal.java
@@ -0,0 +1,8 @@
+package P01_SingleInheritance_LAB;
+
+public class Animal {
+
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ 03_Inheritance/lab/P01_SingleInheritance_LAB/Dog.java
@@ -0,0 +1,8 @@
+package P01_SingleInheritance_LAB;
+
+public class Dog extends Animal {
+
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
