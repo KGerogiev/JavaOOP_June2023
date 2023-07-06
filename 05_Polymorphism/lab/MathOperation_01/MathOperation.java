d747db39a4de61c072dabc744ae75ef1a70b2d58
KGerogiev <kristian_kunev1@abv.bg>
2023-07-06 20:48:54 +0300

    Add code solution

---
+++ 05_Polymorphism/lab/MathOperation_01/MathOperation.java
@@ -0,0 +1,16 @@
+package MathOperation_01;
+
+public class MathOperation {
+
+    public static int add(int first, int second) {
+        return first + second;
+    }
+
+    public static int add(int first, int second, int tirth){
+        return first + second + tirth;
+    }
+
+    public static int add(int first, int second, int tirth, int fourth){
+        return first + second + tirth + fourth;
+    }
+}
